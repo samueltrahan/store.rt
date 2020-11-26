f9eccef1999f7a5fc3ea5b4580da6cb81c514c4b
Samuel Trahan <samtrahan91@gmail.com>
2020-11-25 21:06:12 -0600

    navbar contents added and set up

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -4,7 +4,15 @@ import './NavBar.css';
 export default function NavBar() {
   return (
     <div className="navbar">
-      Nav Bar
+      <div className="logo-section">
+        <img className="logo" alt="" src="/images/store-logo.png"></img>
+      </div>
+      <div className="nav-links">
+        <a className="links" href="/prodcuts">Products</a>
+        <a className="links" href="/services">Service</a>
+        <a className="links" href="/about">About</a>
+        <a className="links" href="/contact">Contact</a>
+      </div>
     </div>
   )
 }
