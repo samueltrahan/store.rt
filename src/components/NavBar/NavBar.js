1fe3dac1cfb55021edc164194c3eaec2f715ef89
Samuel Trahan <samtrahan91@gmail.com>
2020-11-25 21:13:20 -0600

    banner section set up

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -5,11 +5,11 @@ export default function NavBar() {
   return (
     <div className="navbar">
       <div className="logo-section">
-        <img className="logo" alt="" src="/images/store-logo.png"></img>
+        <a href="/"><img className="logo" alt="" src="/images/store-logo.png"></img></a>
       </div>
       <div className="nav-links">
-        <a className="links" href="/prodcuts">Products</a>
-        <a className="links" href="/services">Service</a>
+        <a className="links" href="/products">Products</a>
+        <a className="links" href="/services">Services</a>
         <a className="links" href="/about">About</a>
         <a className="links" href="/contact">Contact</a>
       </div>
