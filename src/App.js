27bf9e51f6f7b42c330176bd2b1fde22db82e8e4
Samuel Trahan <samtrahan91@gmail.com>
2020-11-27 12:11:29 -0600

    nav bar pages added setting up links

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import React from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
 import './App.css';
 import Banner from './components/Banner/Banner';
 import Featured from './components/Featured/Featured';
 import Reason from './components/Reason/Reason';
 import Footer from './components/Footer/Footer';
+import Products from './pages/Products/Products';
 
 export default function App() {
   return (
-    <div>
+    <Router>
       <NavBar />
-      <Banner />
-      <Featured />
-      <Reason />
-      <Footer />
-    </div>
+      <Route exact path="/" render={() => 
+      <div>
+        <Banner />
+        <Featured />
+        <Reason />
+        <Footer />
+      </div>
+      }>
+      </Route>
+      <Route exact path="/products" render={() => 
+      <div>
+          <Products />
+      </div>
+      }>
+      </Route>
+    </Router>
   )
 }
