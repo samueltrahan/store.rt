d605551096000d015bf5d9987ff2429aadaab066
Samuel Trahan <samtrahan91@gmail.com>
2020-11-26 19:37:47 -0600

    banner section contents

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -5,6 +5,7 @@ export default function Banner() {
   return (
     <div className="banner-section">
       <h1 className="store">React Store</h1>
-    </div>
+      <p className="banner-script">Helping you sell your products with fast and responsive sites.</p>
+      </div>
   )
 }
