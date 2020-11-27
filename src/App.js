efde45870fe834a0434647f2d3a6353458cd418b
Samuel Trahan <samtrahan91@gmail.com>
2020-11-27 12:48:07 -0600

    products on product page

--- src/App.js
+++ src/App.js
@@ -10,6 +10,15 @@ import Products from './pages/Products/Products';
 import Services from './pages/Services/Services';
 import About from './pages/About/About';
 import Contact from './pages/Contact/Contact';
+import Pic01 from './Assets/shoe01.jpg';
+import Pic02 from './Assets/shoe02.jpg'
+import Pic03 from './Assets/shoe03.jpg'
+import Pic04 from './Assets/shoe04.jpg'
+import Pic05 from './Assets/shoe05.jpg'
+import Pic06 from './Assets/shoe06.jpg'
+import Pic07 from './Assets/shoe07.jpg'
+import Pic08 from './Assets/shoe08.jpg'
+import Pic09 from './Assets/shoe09.jpg'
 
 export default function App() {
   return (
@@ -26,7 +35,16 @@ export default function App() {
       </Route>
       <Route exact path="/products" render={() => 
       <div>
-          <Products />
+          <h1 className="product-heading">Products</h1>
+          <Products image={Pic01} price="$150.00" />
+          <Products image={Pic02} price="$150.00" />
+          <Products image={Pic03} price="$59.99" />
+          <Products image={Pic04} price="$120.00" />
+          <Products image={Pic05} price="$160.00" />
+          <Products image={Pic06} price="$90.00" />
+          <Products image={Pic07} price="$150.00" />
+          <Products image={Pic08} price="$80.00" />
+          <Products image={Pic09} price="$120.00" />
       </div>
       }>
       </Route>
--- src/pages/Products/Products.js
+++ src/pages/Products/Products.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import './Products.css';
 
-export default function Products() {
+export default function Products({image, price}) {
   return (
-    <div>
-      Prducts Page
-    </div>
+    <Link to="/products/id" className="product-section">
+      <div className="product-card">
+          <div className="image-section">
+            <img className="product-image" alt="" src={image}></img>
+          </div>
+          <div>
+            <h2>{price}</h2>
+          </div>
+      </div>
+    </Link>
   )
 }
