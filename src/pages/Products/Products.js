93e07113533d6c38c76525974ba9e79f3281111d
Samuel Trahan <samtrahan91@gmail.com>
2020-12-02 10:59:21 -0600

    server folder set up

--- src/pages/Products/Products.js
+++ src/pages/Products/Products.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import StripeCheckout from 'react-stripe-checkout';
 import './Products.css';
 
 export default function Products({image, price, id}) {
+
+  const handleToken = (token, addresses) => {
+      console.log({token, addresses})
+  }
+
   return (
     <Link to={id} className="product-section">
       <div className="product-card">
@@ -12,6 +18,12 @@ export default function Products({image, price, id}) {
           <div>
             <h2>{price}</h2>
           </div>
+          <StripeCheckout stripeKey="pk_test_51HtxUMHNTB4gu9aGx5RGz2s3fr5gdUchQxQzs26a7O6yQe1xczX4P3Zt3m3QREvDjTDK9cAu2lMUy6Tl0mX2GSgR00eEsn9dH4"
+          token={handleToken}
+          billingAddress
+          shippingAddress
+          amount={price * 100}
+          />
       </div>
     </Link>
   )
